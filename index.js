cc2fb4b54eae64f98940b2bd674de2571e5fbb88
Noah A <nanderson815@gmail.com>
2019-02-08 17:14:01 -0500

    got the loop to work much faster

--- index.js
+++ index.js
@@ -4,36 +4,34 @@ const url = 'https://www.instagram.com/p/Bs1aNd1AMNf/';
 
 var exists = true;
 
-async function rungProg(){
+async function rungProg() {
     console.log('running');
     var comments = [];
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
-    while (exists === true){
+    while (exists === true) {
         console.log(exists);
         await getComments(page);
     }
     await printComments(page, browser, comments);
     await console.log(comments.length);
-    await browser.close();    
+    await browser.close();
 };
 
 rungProg();
 
 
 async function getComments(page) {
-    try {
-        await Promise.all([
-            await page.waitForSelector('button.Z4IfV'),
-            page.click('button.Z4IfV'),
-            console.log("clicked"),
-            page.waitFor(500),
-        ]);
-    } catch (error) {
+
+    if (await page.$('button.Z4IfV') !== null) {
+        page.click('button.Z4IfV');
+        await console.log("clicked");
+        await page.waitFor(500);
+    } else {
         exists = false;
-        console.log("The element didn't appear.");
+        await console.log('not found');
     }
 };
 
@@ -44,3 +42,22 @@ async function printComments(page, browser, comments) {
         comments.push($(this).text());
     });
 }
+
+
+
+
+
+
+// async function getComments(page) {
+//     try {
+//         await Promise.all([
+//             await page.waitForSelector('button.Z4IfV'),
+//             page.click('button.Z4IfV'),
+//             console.log("clicked"),
+//             page.waitFor(500),
+//         ]);
+//     } catch (error) {
+//         exists = false;
+//         console.log("The element didn't appear.");
+//     }
+// };
