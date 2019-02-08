9b9a7cb5301dc77e6b8d7c5e2eb509ca5111f810
Noah A <nanderson815@gmail.com>
2019-02-08 07:09:01 -0500

    refactored to make loop

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const $ = require('cheerio');
 const url = 'https://www.instagram.com/p/BtY0Bsrg1mS/';
 
 
-(async () => {
+async function rungProg(){
+    console.log('running');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    await Promise.all([
-        page.click('button.Z4IfV'),
-        console.log("clicked"),
-        page.waitFor(500),
-        page.click('button.Z4IfV'),
-    ]);
+
+    await getComments(page);
+    await printComments(page, browser);
+};
+
+rungProg();
+
+async function getComments(page) {
+    try {
+        await Promise.all([
+            await page.waitForSelector('button.Z4IfV'),
+            page.click('button.Z4IfV'),
+            console.log("clicked"),
+            page.waitFor(500),
+        ]);
+    } catch (error) {
+        console.log("The element didn't appear.")
+    }
+};
+
+async function printComments(page, browser) {
+
     const html = await page.content()
-    await $('h3 > a', html).each(function(){
+    await $('h3 > a', html).each(function () {
         console.log($(this).text());
     });
-
     await browser.close();
-})();
+}
