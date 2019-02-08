a7cb9a731f211e4a19e285c139544ef09d7df0b1
Noah A <nanderson815@gmail.com>
2019-02-07 19:10:38 -0500

    added a button click. need to loop until all loaded

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-const url = 'https://www.instagram.com/p/BtiiYTSggiA/';
+const url = 'https://www.instagram.com/p/BtY0Bsrg1mS/';
 
-puppeteer
-    .launch()
-    .then(function (browser) {
-        return browser.newPage();
-    })
-    .then(function (page) {
-        return page.goto(url).then(function () {
-            return page.content();
-        });
-    })
-    .then(function (html) {
-        $('h3 > a', html).each(function () {
-            console.log($(this).text());
-        });
-    })
-    .catch(function (err) {
-        //handle error
+// puppeteer
+//     .launch()
+//     .then(function (browser) {
+//         return browser.newPage();
+//     })
+//     .then(function (page) {
+//         return page.goto(url).then(function () {
+//             return page.content();
+//         });
+//     })
+//     .then(function (html) {
+//         $('h3 > a', html).each(function () {
+//             console.log($(this).text());
+//         });
+//     })
+//     .catch(function (err) {
+//         //handle error
+//     });
+
+
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await Promise.all([
+        page.click('button.Z4IfV'),
+        console.log("clicked")
+    ]);
+    const html = await page.content()
+    await $('h3 > a', html).each(function(){
+        console.log($(this).text());
     });
+
+
+
+    await browser.close();
+})();
