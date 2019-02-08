17b69c726d6c69fcc83c52d0f4573d6d233b1044
Noah A <nanderson815@gmail.com>
2019-02-07 20:13:40 -0500

    added a wait and another click, works. Need to loop

--- index.js
+++ index.js
@@ -2,26 +2,6 @@ const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const url = 'https://www.instagram.com/p/BtY0Bsrg1mS/';
 
-// puppeteer
-//     .launch()
-//     .then(function (browser) {
-//         return browser.newPage();
-//     })
-//     .then(function (page) {
-//         return page.goto(url).then(function () {
-//             return page.content();
-//         });
-//     })
-//     .then(function (html) {
-//         $('h3 > a', html).each(function () {
-//             console.log($(this).text());
-//         });
-//     })
-//     .catch(function (err) {
-//         //handle error
-//     });
-
-
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -29,14 +9,14 @@ const url = 'https://www.instagram.com/p/BtY0Bsrg1mS/';
     await page.goto(url);
     await Promise.all([
         page.click('button.Z4IfV'),
-        console.log("clicked")
+        console.log("clicked"),
+        page.waitFor(500),
+        page.click('button.Z4IfV'),
     ]);
     const html = await page.content()
     await $('h3 > a', html).each(function(){
         console.log($(this).text());
     });
-
-
 
     await browser.close();
 })();
