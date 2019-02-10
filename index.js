bb0f09364fd2b49e166e92eee7c379713b552e7e
Noah A <nanderson815@gmail.com>
2019-02-09 21:09:30 -0500

    showed comments loaded while operating

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const $ = require('cheerio');
 var express = require('express')
 var app = express()
 
-const url = 'https://www.instagram.com/p/BtY0Bsrg1mS/';
+const url = 'https://www.instagram.com/p/BrsUeQxFHqa/';
 
 var exists = true;
 var comments = [];
@@ -20,7 +20,7 @@ async function rungProg() {
         await getComments(page);
     }
 
-    await printComments(page, browser, comments);
+    // await printComments(page, comments);
     await console.log(comments);
     await console.log(comments.length);
     await browser.close();
@@ -31,9 +31,11 @@ rungProg();
 // If the "load more" button exists, it will be pressed until all comments have loaded. 
 async function getComments(page) {
     if (await page.$('button.Z4IfV') !== null) {
-        page.click('button.Z4IfV');
+        await page.click('button.Z4IfV');
         await console.log("clicked");
         await page.waitFor(250);
+        comments = [];
+        await printComments(page, comments);
     } else {
         exists = false;
         await console.log('not found');
@@ -41,9 +43,10 @@ async function getComments(page) {
 };
 
 // Stores the comments to the comments array by grabbing internal text.
-async function printComments(page, browser, comments) {
+async function printComments(page, comments) {
     const html = await page.content()
     await $('h3 > a', html).each(function () {
         comments.push($(this).text());
     });
+    await console.log(comments.length);
 }
